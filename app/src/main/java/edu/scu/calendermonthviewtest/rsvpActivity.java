95782b5e2cb655a654c11645cada6f28ef8f0952
nithi <nvasudevan@scu.edu>
2017-05-31 20:00:09 -0700

    before merging two projects

--- app/src/main/java/edu/scu/calendermonthviewtest/rsvpActivity.java
+++ app/src/main/java/edu/scu/calendermonthviewtest/rsvpActivity.java
@@ -1,11 +1,14 @@
 package edu.scu.calendermonthviewtest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static edu.scu.calendermonthviewtest.R.id.button;
 import static edu.scu.calendermonthviewtest.R.id.editText2;
@@ -25,26 +28,26 @@ public class rsvpActivity extends AppCompatActivity {
         text1.setText(bundle.getString("EventDetails"));
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                text1.setText("You have succesfully registered for the event");
-            //    sendEmail();
+                text2.setText("You have succesfully registered for the event");
+            sendEmail();
             }
         });
 
     }
-//    private void sendEmail(){
+   private void sendEmail(){
 //
-//        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
-//        emailIntent.setData(Uri.parse("mailto:" + "recipient@example.com"));
-//        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My email's subject");
-//        emailIntent.putExtra(Intent.EXTRA_TEXT, "My email's body");
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:" + "nvasudevan@scu.edu"));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My email's subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "My email's body");
 //
-//        try {
-//            startActivity(Intent.createChooser(emailIntent, "Send email using..."));
-//        } catch (android.content.ActivityNotFoundException ex) {
-//            Toast.makeText(rsvpActivity.this, "No email clients installed.", Toast.LENGTH_SHORT).show();
-//        }
-//
-//    }
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send email using..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(rsvpActivity.this, "No email clients installed.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 //    private void sendEmail() {
 //        Intent intent = new Intent(Intent.ACTION_SENDTO)
 //                .setData(new Uri.Builder().scheme("mailto").build())
